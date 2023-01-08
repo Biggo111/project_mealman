12679a67ae4d0b437de4593df86fb71e56f78de4
Biggo111 <routh.biggo123@gmail.com>
2023-01-09 02:47:48 +0600

    Images and colors added 1

---
+++ lib/app/core/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color mainColor = Color(0xFE7C00);
+  static const Color backgroundColor = Color(0xEDDFDF);
+  static const Color textColorDark = Color(0x000000);
+}
